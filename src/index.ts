e6bc22d68332bdb4f4accc6d06a50d96ea6f7d90
IzumiSy <beetle-noise@gmx.com>
2022-07-26 19:19:33 +0900

    Implemented field update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,81 @@
-console.log("hello")
+// console.log("hello")
+
+type EventType
+  = "put"
+  | "patch"
+
+interface EventData {
+  path: string;
+  data: Object;
+}
+
+interface Event {
+  event: EventType;
+  data: EventData;
+}
+
+export class SyncObject {
+  private manipulator: SyncObjectManipulator;
+
+  constructor(payload: Event) {
+    const data = payload.data.data
+    if (isObject(data)) {
+      this.manipulator = new ObjectValueManipulator(data)
+    } else {
+      this.manipulator = new PrimitiveValueManipulator(data as PossbilePrimitive)
+    }
+  }
+
+  applyEvent(payload: Event) {
+    this.manipulator.applyEvent(payload);
+  }
+
+  getObject(path: string): unknown {
+    return this.manipulator.getObject(path)
+  }
+}
+
+const isObject = (test: unknown): test is { [key: string]: unknown } => {
+  return typeof test === 'object';
+}
+
+interface SyncObjectManipulator {
+  applyEvent(payload: Event): void;
+  getObject(path: string): unknown;
+}
+
+class ObjectValueManipulator implements SyncObjectManipulator {
+  constructor(private data: { [key: string]: unknown }) { }
+
+  applyEvent(payload: Event): void {
+    const path = payload.data.path.startsWith('/')
+      ? payload.data.path.slice(1) : payload.data.path
+    switch (payload.event) {
+      case "put": {
+        this.data[path] = payload.data.data
+      }
+      case "patch": {
+
+      }
+      default:
+        // 残りはkeep-aliveとかcancelとかがくるはずだが
+        // 更新処理ではないため意図的に無視する
+        return;
+    }
+  }
+
+  getObject(path: string): unknown {
+    return this.data[path];
+  }
+}
+
+type PossbilePrimitive = boolean | number | string;
+class PrimitiveValueManipulator implements SyncObjectManipulator {
+  constructor(private data: PossbilePrimitive) { }
+
+  applyEvent(): void { }
+
+  getObject(path: string): unknown {
+    return this.data
+  }
+}
--- test.mjs
+++ test.mjs
@@ -1,5 +1,115 @@
 import test from 'ava';
+import { SyncObject } from './dist/index.js';
 
-test('index', t => {
-  t.pass();
+test('noop', t => {
+  const syncObject = new SyncObject({
+    event: "put",
+    data: {
+      path: "/",
+      data: {
+        foo: 10,
+        bar: 20,
+        baz: 30,
+      },
+    },
+  })
+
+  const result = syncObject.getObject("bar")
+  t.is(result, 20)
+})
+
+test('replacing a field', t => {
+  const syncObject = new SyncObject({
+    event: "put",
+    data: {
+      path: "/",
+      data: {
+        foo: 10,
+        bar: 20,
+        baz: 30,
+      },
+    },
+  })
+
+  syncObject.applyEvent({
+    event: "put",
+    data: {
+      path: "/foo",
+      data: 15,
+    }
+  })
+
+  const result = syncObject.getObject("foo")
+  t.is(result, 15);
+});
+
+test('replacing a field in objects', t => {
+  const syncObject = new SyncObject({
+    event: "put",
+    data: {
+      path: "/",
+      data: {
+        foo: {
+          name: "justine",
+          age: 20,
+        },
+        bar: {
+          name: "michael",
+          age: 25,
+        },
+      },
+    },
+  })
+
+  syncObject.applyEvent({
+    event: "put",
+    data: {
+      path: "/foo/name",
+      data:  "alexandor",
+    }
+  })
+
+  const result = syncObject.getObject("foo")
+  t.is(result, {
+    name: "alexandor",
+    age: 20,
+  });
+});
+
+test('appending a child', t => {
+  const syncObject = new SyncObject({
+    event: "put",
+    data: {
+      path: "/",
+      data: {
+        foo: {
+          name: "justine",
+          age: 20,
+        },
+        bar: {
+          name: "michael",
+          age: 25,
+        },
+      },
+    },
+  })
+
+  syncObject.applyEvent({
+    event: "patch",
+    data: {
+      path: "/foo",
+      data: {
+        baz: {
+          name: "alexandor",
+          age: 30,
+        },
+      },
+    }
+  })
+
+  const result = syncObject.getObject("baz")
+  t.is(result, {
+    name: "alexandor",
+    age: 30,
+  });
 });
