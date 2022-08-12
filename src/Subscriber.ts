b735ecaa2ca70bd0785de7dfcf7991d7453ec0cf
IzumiSy <beetle-noise@gmx.com>
2022-08-12 12:26:48 +0900

    wip

--- src/Subscriber.ts
+++ src/Subscriber.ts
@@ -8,30 +8,30 @@ interface EventSourceLike {
 const createEventSource = (url: string): EventSourceLike =>
   new EventSource(url);
 
+
 class Synchronizer {
   private syncObject?: SyncObject;
   private eventSource: EventSourceLike;
 
-  constructor(url: string, connector: (url: string) => EventSourceLike = createEventSource) {
+  constructor(
+    url: string,
+    connector: (url: string) => EventSourceLike = createEventSource,
+  ) {
     this.eventSource = connector(url);
     this.eventSource.addEventListener('open', this.onOpen.bind(this))
-    this.eventSource.addEventListener('put', this.onPut.bind(this))
-    this.eventSource.addEventListener('patch', this.onPatch.bind(this))
+    this.eventSource.addEventListener('put', e => this.onUpdate('put', e))
+    this.eventSource.addEventListener('patch', e => this.onUpdate('patch', e))
     this.eventSource.addEventListener('error', this.onError.bind(this))
   }
 
   private onOpen(event: MessageEvent<EventData>) {
+    // open event sends an initial data for the subscribing path.
     this.syncObject = new SyncObject(event)
   }
 
-  private onPut(event: MessageEvent<EventData>) {
+  private onUpdate(type: 'put' | 'patch', event: MessageEvent<EventData>) {
     if (!this.syncObject) return
-    // TODO
-  }
-
-  private onPatch(event: MessageEvent<EventData>) {
-    if (!this.syncObject) return
-    // TODO
+    const result = this.syncObject.applyEvent(type, event);
   }
 
   private onError(event: MessageEvent<EventData>) {
